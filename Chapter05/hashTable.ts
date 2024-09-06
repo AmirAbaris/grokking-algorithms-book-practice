4cd0bbc0df22911ebfeb071966c29bc4d788964d
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-09-06 11:30:14 +0330

    [UPDATE] added hash table example

---
+++ Chapter05/hashTable.ts
@@ -0,0 +1,15 @@
+// i used typescript for this because its my main language of choice
+interface Person {
+    name: string
+}
+
+interface HashTable<T> {
+    [key: string]: T;
+}
+
+// init the hash table
+var person: HashTable<Person> = {};
+
+person["John"] = { name: "John" }
+
+console.log(person["John"].name);
